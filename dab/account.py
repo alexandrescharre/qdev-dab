d3129634b49cd825a94d3a8a8f931414176ffe4e
Marek Felšöci <marek@felsoci.sk>
2026-03-11 22:02:50 +0100

    Allow for identifying an account with account number

--- dab/account.py
+++ dab/account.py
@@ -12,6 +12,12 @@ class Account:
   def __str__(self):
     return f"Account {self.number}:\n  - holder: {self.client}\n  - balance: {self.balance}\n"
 
+  def __eq__(self, other):
+    if self.number == other:
+      return True
+    else:
+      return False
+
   def deposit(self, amount):
     self.balance += amount
     print("Deposit complete. New balance is %.2f" % self.balance)
