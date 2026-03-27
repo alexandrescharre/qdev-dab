bd8dab585710643ac43ca9677d7dbe66cd22c23f
Alexandre Scharre <22402148@me.uvsq.fr>
2026-03-27 11:58:14 +0100

    Tests créés

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,7 +3,10 @@ from dab.account import *
 
 class checkWithdrawalUnitTests(unittest.TestCase):
   def test_authorized(self):
-    self.assertEqual(True, False)
+    self.assertEqual(checkWithdrawal(600,450), True)
+    self.assertEqual(checkWithdrawal(300, 450), False)
+    self.assertEqual(checkWithdrawal(999, 501), False)
+    self.assertRaises(ValueError, checkWithdrawal, -1)
 
 if __name__ == '__main__':
   unittest.main()
